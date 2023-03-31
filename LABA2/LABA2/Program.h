1de313baeb5bff40b87b265b63bf292ce82ff875
leonnik28 <112748954+leonnik28@users.noreply.github.com>
2023-03-31 18:06:17 +0300

    Update Program.h

--- LABA2/LABA2/Program.h
+++ LABA2/LABA2/Program.h
@@ -5,17 +5,17 @@
 int search_popular_max_int(NodeWord* node);
 void file_open_all(errno_t err, errno_t err_1);
 void file_open(errno_t err);
-void createArray(NodeWord* pack, char* wd);
+void create_struct(NodeWord* pack, const char* wd);
 NodeWord* createArrayNode();
+void init_struct(NodeWord* node, char* token);
 void searc_word(NodeWord* node);
 void to_lower(char* str);
 char* search_popular_min(NodeWord* node, int count_max);
 char* search_popular_max(NodeWord* node);
-void inputSpaces(unsigned const char* buffer, char* temp, int i);
 void transition_new_line(char* temp, const char* token);
 void delete_last_symbol(unsigned char* temp);
 void token(char* buffer, char* tmp, char** words, int end);
 int compress(NodeWord* node);
-void init_array(NodeWord* node, char*** word, FILE* fp_out);
-int init_array_int(NodeWord* node, char** word, FILE* fp_out);
+void init_array(char*** word, FILE* fp_out);
+int init_array_int(char** word, FILE* fp_out);
 #endif
